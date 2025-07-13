1b4f124b019114874e53c43f7d721c3c9de5842f
Revanth <20epci032@skcet.ac.in>
2025-07-13 23:31:26 +0530

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,7 +21,7 @@ export default function contact () {
         <label htmlFor="message">Message</label>
         <textarea id="message" name="message" required></textarea>
 
-          <input type="hidden" name="_next" value="#" />
+          <input type="hidden" name="_next" value="https://portfolio-azure-zeta-10.vercel.app/contact" />
           <input type="hidden" name="_captcha" value="false" />
         <button type="submit" className={styles.button}>Send</button>
       </form>
